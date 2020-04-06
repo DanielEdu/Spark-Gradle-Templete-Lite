6a02aaf16378058082d8b070b9655c8685d27011
daniel <daniel.epr77@gmail.com>
2020-04-06 11:42:19 -0500

    template inicial | Spark + Scala + Gradle

---
+++ src/main/scala/template/spark/Main.scala
@@ -0,0 +1,35 @@
+package template.spark
+
+import org.apache.spark.sql.functions._
+
+final case class Person(firstName: String, lastName: String,
+                        country: String, age: Int)
+
+object Main extends InitSpark {
+
+  def main(args: Array[String]): Unit = {
+    import spark.implicits._
+
+    val sparkVersion = spark.version
+    val scalaVersion = util.Properties.versionNumberString
+
+    println("SPARK VERSION = " + sparkVersion)
+    println("SCALA VERSION = " + scalaVersion)
+
+
+    val listRdd = spark.range(1, 101)
+    println("Sum 1 to 100 using binary : "+listRdd.reduce(_ + _))
+
+
+
+    println("Reading from csv file: people-example.csv")
+    val persons = reader.csv("people-example.csv").as[Person]
+    persons.show(2,false)
+
+    val averageAge = persons.agg(avg("age"))
+                     .first.get(0).asInstanceOf[Double]
+    println(f"Average Age: $averageAge%.2f")
+
+    close
+  }
+}
